53a841e84f7cdeed1fdb04ed30cee35360776ba1
Max Beringer <max.beringer@student.unisg.ch>
2025-03-14 01:01:01 +0100

    fix: deployment script

--- deploy_to_hf.py
+++ deploy_to_hf.py
@@ -6,11 +6,19 @@ MODEL_FILE = "model_artifacts/model.pt"
 HF_TOKEN = os.environ["HF_TOKEN"]
 
 api = HfApi()
-api.upload_file(
-    path_or_fileobj=MODEL_FILE,
-    path_in_repo="model.pt",
-    repo_id=HF_REPO_ID,
-    repo_type="model",
-    token=HF_TOKEN,
-)
+
+files_to_upload = {
+    "app.py": "app.py",
+    "model.py": "model.py",
+    "model_artifacts/model.pt": "model.pt"
+}
+
+for local_path, repo_path in files_to_upload.items():
+    api.upload_file(
+         path_or_fileobj=local_path,
+         path_in_repo=repo_path,
+         repo_id=HF_REPO_ID,
+         repo_type="space",
+         token=HF_TOKEN,
+    )
 print("Model deployed to Hugging Face")
