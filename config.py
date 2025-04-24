91a3885887f321c96cf7d0af74db3741325102bd
Max Beringer <max.beringer@student.unisg.ch>
2025-04-24 18:07:24 +0200

    feat: add new workflows for automation

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ load_dotenv()
 # set global experiment config
 
 # experiment and model names
-EXPERIMENT_NAME = os.getenv("MLFLOW_EXPERIMENT_NAME", "alexnet_auto_deploy_test-4")
-REGISTERED_MODEL_NAME = os.getenv("MLFLOW_MODEL_NAME", "alexnet_model-4")
+EXPERIMENT_NAME = os.getenv("MLFLOW_EXPERIMENT_NAME", "alexnet_auto_deploy_test-1")
+REGISTERED_MODEL_NAME = os.getenv("MLFLOW_MODEL_NAME", "alexnet_model-1")
 RUN_NAME = os.getenv("MLFLOW_RUN_NAME", "challenger")
 
 # model registry aliases
