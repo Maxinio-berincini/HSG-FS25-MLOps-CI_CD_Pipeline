3c91699504e87e92ee7bffbbc213400d4d121b41
Max Beringer <max.beringer@student.unisg.ch>
2025-04-28 18:22:12 +0200

    refactor: reformat file

--- evaluate_and_promote.py
+++ evaluate_and_promote.py
@@ -14,7 +14,7 @@ from data_utils import test_iterator
 # load config
 from config import REGISTERED_MODEL_NAME, CHALLENGER_ALIAS, PRODUCTION_ALIAS
 
-## load data_utils
+# load data_utils
 from data_utils import is_test_set_sufficiently_large
 
 # set up device & loss
@@ -24,9 +24,10 @@ CRITERION = nn.CrossEntropyLoss()
 mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI"))
 client = MlflowClient()
 
-## method to evaluate model with confidence intervals following ease.ml
+
+# method to evaluate model with confidence intervals following ease.ml
 def evaluate_model_with_confidence_interval(model_uri, confidence=0.9999):
-    #load the model
+    # load the model
     model = mlflow.pytorch.load_model(model_uri, map_location=DEVICE)
     model.to(DEVICE)
     model.eval()
@@ -39,7 +40,8 @@ def evaluate_model_with_confidence_interval(model_uri, confidence=0.9999):
             outputs = model(inputs)
 
             if isinstance(outputs, tuple):
-                outputs = outputs[0] #first element is logits
+                # first element is logits
+                outputs = outputs[0]
 
             _, preds = torch.max(outputs, 1)
             all_preds.extend(preds.cpu().numpy())
@@ -47,7 +49,7 @@ def evaluate_model_with_confidence_interval(model_uri, confidence=0.9999):
 
     sample_accuracies = [1 if pred == label else 0 for pred, label in zip(all_preds, all_labels)]
 
-    ## using bootstrap method to calculate confidence interval 
+    # using bootstrap method to calculate confidence interval
     n_bootstrap = 10000
     bootstrap_accuracies = []
     n_samples = len(sample_accuracies)
@@ -67,23 +69,24 @@ def evaluate_model_with_confidence_interval(model_uri, confidence=0.9999):
         "mean_accuracy": mean_accuracy,
         "lower_bound": lower_bound,
         "upper_bound": upper_bound,
-        "eval_epsilon": (upper_bound - lower_bound) / 2, # half-width of the confidence interval == actual observed error 
+        # half-width of the confidence interval == actual observed error
+        "eval_epsilon": (upper_bound - lower_bound) / 2,
         "confidence": confidence
     }
 
-# method to compare statistics to be significant
-def is_model_significantly_better(challenger_metrics, production_metrics, min_acc_improvement=0.01, allow_non_sig_improvements = False):
-    
-    effective_min_improvent = max(min_acc_improvement, 2 * challenger_metrics['eval_epsilon'])
 
+# method to compare statistics to be significant
+def is_model_significantly_better(challenger_metrics, production_metrics, min_acc_improvement=0.01,
+                                  allow_non_sig_improvements=False):
+    effective_min_improvent = max(min_acc_improvement, 2 * challenger_metrics['eval_epsilon'])
 
     # Check if the confidence intervals overlap
     if challenger_metrics['lower_bound'] > production_metrics['upper_bound']:
         return True, "Challenger is significantly better"
 
     if challenger_metrics['mean_accuracy'] - production_metrics['mean_accuracy'] > effective_min_improvent:
-        return True, "Challenger is better however not significant" if allow_non_sig_improvements else False , "Challenger is not significantly better"
-    
+        return True, "Challenger is better however not significant" if allow_non_sig_improvements else False, "Challenger is not significantly better"
+
     return False, "Challenger is not significantly better than production"
 
 
@@ -91,15 +94,15 @@ def is_model_significantly_better(challenger_metrics, production_metrics, min_ac
 challenger_mv = client.get_model_version_by_alias(REGISTERED_MODEL_NAME, alias=CHALLENGER_ALIAS)
 challenger_uri = f"models:/{REGISTERED_MODEL_NAME}@{CHALLENGER_ALIAS}"
 
+# testing with confidence intervals and statistical significance
 
-## testing with confidence intervals and statistical significance
-
-EPSILON = 0.02 # 2% error tolerance
-CONFIDENCE = 0.99999 # 99.99% confidence level
-DELTA = 1-CONFIDENCE # 99.99% confidence level
+EPSILON = 0.02  # 2% error tolerance
+CONFIDENCE = 0.90  # 99.99% confidence level
+DELTA = 1 - CONFIDENCE  # 99.99% confidence level
 
 # first, check if test set is large enough
-is_sufficient, test_set_size, required_size = is_test_set_sufficiently_large(EPSILON, DELTA, test_iterator= test_iterator)
+is_sufficient, test_set_size, required_size = is_test_set_sufficiently_large(EPSILON, DELTA,
+                                                                             test_iterator=test_iterator)
 if not is_sufficient:
     print(f"Test set size {test_set_size} is not sufficient for epsilon={EPSILON}, delta={DELTA}. "
           f"Required size: {required_size}.")
@@ -112,7 +115,8 @@ print(f"Challenger model accuracy: {challenger_metrics['mean_accuracy']:.3f} "
       f"± {challenger_metrics['eval_epsilon']:.3f} (confidence: {challenger_metrics['confidence']})")
 
 if challenger_metrics['eval_epsilon'] > EPSILON:
-    print(f"Warning: Achieved precision ({challenger_metrics['eval_epsilon']:.4f}) is worse than target ({EPSILON:.4f})")
+    print(
+        f"Warning: Achieved precision ({challenger_metrics['eval_epsilon']:.4f}) is worse than target ({EPSILON:.4f})")
 
 # then, evaluate production model with statistical significance if exists
 try:
@@ -123,8 +127,9 @@ except Exception:
     production_metrics = {'accuracy': -1.0, 'lower_bound': -1.0, 'upper_bound': -1.0}
     print("No production model found.")
 
-## compare models with statistical guarantees
-is_better, message = is_model_significantly_better(challenger_metrics, production_metrics, allow_non_sig_improvements=False)
+# compare models with statistical guarantees
+is_better, message = is_model_significantly_better(challenger_metrics, production_metrics,
+                                                   allow_non_sig_improvements=False)
 
 if is_better:
 
@@ -142,5 +147,6 @@ if is_better:
     print(f"Promoted version {challenger_mv.version} to production (score {challenger_metrics['mean_accuracy']:.3f})")
     exit(0)
 else:
-    print(f"Challenger ({challenger_metrics['mean_accuracy']:.3f}) ≤ production ({production_metrics['mean_accuracy']:.3f}), not promoted")
+    print(
+        f"Challenger ({challenger_metrics['mean_accuracy']:.3f}) ≤ production ({production_metrics['mean_accuracy']:.3f}), not promoted")
     exit(1)
